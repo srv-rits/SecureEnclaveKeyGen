d81cdf52fcd42765643688fdb720f993ebcf67ac
Sourav Mishra <sourav@swnglobal.com>
2024-02-26 18:03:46 +0530

    Initial Commit

---
+++ SecureEnclaveKeyGen/SecureEnclaveKeyGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureEnclaveKeyGenApp.swift
+//  SecureEnclaveKeyGen
+//
+//  Created by Sourav Mishra on 26/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SecureEnclaveKeyGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
